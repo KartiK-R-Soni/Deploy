9e7418445fbb8adc300a653e58b3df22f2ed2654
Kartik <kartiksoni21@iitk.ac.in>
2023-04-18 00:14:57 +0530

    ic

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,7 @@ const app = express()
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 app.use(cors({
-    origin : "http://localhost:3000",
+    origin : "https://kartikisaswesome.web.app",
     credentials : true
 }))
 app.use(cookieParser())
